7600eb330a2760e682d0906f39f232a80b842ade
Greg De La Torre <gdlt5@nmsu.edu>
2017-04-18 13:21:29 -0600

    Add Goblin.java & edited current Monster classes

---
+++ Goblin.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Goblin extends Monster
+{
+   public Goblin(){
+      super("Goblin", 12, 3, 2, 10);
+      Equipment goblinMace = new Equipment("Goblin Mace", 2, 100, 0, 1, 0, 0, "P", "A poorly constructed mace. It feels as though it was used for " + 
+         "purposes beyond its intended use.  ATTACK +1.", 1, 6);
+      Equipment goblinDagger = new Equipment("Goblin Dagger", 2, 150, 0, 2, 0, 0, "P", "A dagger held by high-status goblins." + 
+         "While dull, it is a sybol of status amongst their ranks.  ATTACK +2", 1, 3);
+      Consumable bread = new Consumable("Bread", 1, 5, 2,0,0,0, "Regular Ol' Bread. The choice meal of every Navy Seal.", 2);
+      Consumable bread2 = new Consumable("Strange Bread", 1, 5, 2,0,0,0, "Regular Ol' Bread, but slightly different. The choice meal of every Navy Seal.", 2);
+	  LootTable temp = new LootTable(3);
+	  possibleLoot = temp;
+	  possibleLoot.add(goblinMace, 45);
+	  possibleLoot.add(goblinDagger, 30);
+	  possibleLoot.add(bread, 50);
+	  possibleLoot.add(bread2, 2);
+	  createInventory();
+      
+   }
+	
+	public void print()
+	{
+		System.out.println("Create a Goblin, you lazy twat.");
+
+	}
+}
+/*
+
+*/
--- Skeleton.java
+++ Skeleton.java
@@ -8,11 +8,11 @@ public class Skeleton extends Monster
    public Skeleton(){
       super("Skeleton", 10, 4, 2, 5);
       Equipment skeletonSword = new Equipment("Skeleton Sword", 1, 200, 0, 4, 0, 0, "P", "A skeleton sword. Its " + 
-         "Bony edge is rather sharp.  ATTACK +4", 1, 1);
-      Equipment skeletonBow = new Equipment("Skeleton Flail", 1, 200, 1, 3, 1, 0, "P", "A Flail made of bones...  HEALTH +1 " + 
+         "scuplted bone edge is rather sharp.  ATTACK +4", 1, 1);
+      Equipment skeletonBow = new Equipment("Skeleton Flail", 1, 200, 1, 3, 1, 0, "P", "A flail made of bones...  HEALTH +1 " + 
          ": ATTACK +3 : DEFENSE +1", 1, 5);
       Consumable bread = new Consumable("Bread", 1, 5, 2,0,0,0, "Regular Ol' Bread. The choice meal of every Navy Seal.", 2);
-      Consumable bread2 = new Consumable("Bread2", 1, 5, 2,0,0,0, "Regular Ol' Bread, but slightly different. The choice meal of every Navy Seal.", 2);
+      Consumable bread2 = new Consumable("Strange Bread", 1, 5, 2,0,0,0, "Regular Ol' Bread, but slightly different. The choice meal of every Navy Seal.", 2);
 	  LootTable temp = new LootTable(3);
 	  possibleLoot = temp;
 	  possibleLoot.add(skeletonSword, 45);
@@ -21,21 +21,9 @@ public class Skeleton extends Monster
 	  possibleLoot.add(bread2, 2);
 	  createInventory();
       
-      /* for(int i = 0; i < inventory.length; i++){
-         Random rand = new Random();
-		   int randomInt = rand.nextInt(8 - 1 + 1) + 1;
-         if(randomInt <= 2)
-            inventory[i] = skeletonSword;
-         else if(randomInt > 2 && randomInt <= 4)
-            inventory[i] = skeletonBow;
-         else if(randomInt > 4 && randomInt <= 6)
-            inventory[i] = bread;
-         else if(randomInt >6)
-            inventory[i] = null; 
-      }*/
    }
 	
-	public void printSkeleton()
+	public void print()
 	{
 		System.out.println();
 		System.out.println("        .-.");
